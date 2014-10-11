24a8e8eee7d17b183dbc94076bbe66c0325c483e
Eric C. Brown <djheir@gmail.com>
2014-10-11 14:04:37 -0500

    Updates back to home button click handler to clear cookies.  Also resets height of webview back to 100%.

--- window.js
+++ window.js
@@ -2,8 +2,15 @@ window.onload = function() {
   var home = 'http://thejourney.org/node/21877'
     , webview = document.querySelector("webview");
 
+  // Set the initial src of webview.
   webview.src = home;
+
+  /**
+   * Click handler for "back to home button".
+   */
   document.querySelector("#backToHomBtn").onclick = function() {
-    webview.src = home;
+    webview.clearData({since:0}, {cookies: true}, function() {
+      webview.src = home;
+    });
   }
 }
